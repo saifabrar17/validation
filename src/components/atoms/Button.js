9f621072da8d031c02d9c5b88d70569366aee54c
Saif Abrar <saifabrar.1998@gmail.com>
2023-03-25 14:25:22 +0600

    validation working

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = () => {
-  return <button type="submit">Submit</button>;
+  return <button className="bg-green-500 p-3 rounded-md text-white" type="submit">Submit</button>;
 };
 
 export default Button;
--- src/components/molecules/Form.js
+++ src/components/molecules/Form.js
@@ -1,18 +1,95 @@
-import React from 'react';
-import Button from '../atoms/Button';
-import Input from '../atoms/Input';
+import React, { useState } from "react";
+import Button from "../atoms/Button";
 
-const form = () => {
-    return (
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [formErrors, setFormErrors] = useState({});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    const errors = validate(formData);
+    setFormErrors(errors);
+
+    if (Object.keys(errors).length === 0) {
+      console.log(formData);
+    }
+  };
+
+  const validate = (data) => {
+    const errors = {};
+    if (!data.name) {
+      errors.name = "Name is required";
+    }
+    if (!data.email) {
+      errors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+      errors.email = "Email is invalid";
+    }
+    if (!data.password) {
+      errors.password = "Password is required";
+    } else if (data.password.length < 6) {
+      errors.password = "Password must be at least 6 characters";
+    }
+    return errors;
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    setFormErrors({
+      ...formErrors,
+      [name]: "",
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
         <div>
-            <div>
-                <form className='' >
-                      <Input />
-                      <Button />
-                </form>
-            </div>
+          <label className="block text-gray-700 text-sm font-bold mb-2">Name</label>
+          <input
+            type="text"
+            name="name"
+            className="shadow border rounded w-full py-2 px-3 text-gray-700"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+          {formErrors.name && <span className="text-red-500 text-base">{formErrors.name}</span>}
         </div>
-    );
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+          {formErrors.email && <span>{formErrors.email}</span>}
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+          {formErrors.password && <span>{formErrors.password}</span>}
+        </div>
+        {/* <button type="submit" className="bg-green-500 p-3 rounded-md text-white">Submit</button> */}
+        <Button />
+      </form>
+    </div>
+  );
 };
 
-export default form;
+export default Form;
